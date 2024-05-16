e9639d1ee46ea9496927220e59d9ab269b95f912
arief sangga <arief.heaven@gmail.com>
2024-05-16 15:25:35 +0700

    update modules and utils

---
+++ src/utils/entity-ref-abstract.entity.ts
@@ -0,0 +1,14 @@
+import { IsUUID } from "class-validator";
+import { PrimaryGeneratedColumn } from "typeorm";
+
+export type ObjectRef = {
+    id: string;
+}
+
+export abstract class EntityRef<T extends EntityRef = ObjectRef>
+    implements ObjectRef
+{
+    @IsUUID('4')
+    @PrimaryGeneratedColumn('uuid')
+    id: string
+}
